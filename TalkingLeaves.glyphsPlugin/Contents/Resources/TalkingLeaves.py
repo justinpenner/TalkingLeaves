ca30f038b9f5367e3c84a85089cb1dacaa995d05
Justin Penner <46039243+justinpenner@users.noreply.github.com>
2024-06-20 08:38:33 -0700

    Fix Hyperglot update checker
    JSON data downloaded from pypi.org was being decoded from binary to string twice, raising an error the second time that was being caught and silenced.

--- TalkingLeaves.glyphsPlugin/Contents/Resources/TalkingLeaves.py
+++ TalkingLeaves.glyphsPlugin/Contents/Resources/TalkingLeaves.py
@@ -668,7 +668,7 @@ class TalkingLeaves:
 
     def callback(data):
       try:
-        metadata = utils.parseJson_(data.decode('utf-8'))
+        metadata = utils.parseJson_(data)
       except Exception:
         # Not critical, so if anything goes wrong we can just check for updates again on next launch
         return
